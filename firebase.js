0bec961b87748e887aa6df287762fe04452778e5
Niveditha-K17 <75623255+Niveditha-K17@users.noreply.github.com>
2021-11-16 23:37:55 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCR2k68AlutC4EkBy3HQB9z-OxiqpnvWXI",
+    authDomain: "clone-minip.firebaseapp.com",
+    projectId: "clone-minip",
+    storageBucket: "clone-minip.appspot.com",
+    messagingSenderId: "558841466924",
+    appId: "1:558841466924:web:7b99dd62da44962545c189",
+    measurementId: "G-FK2HVB52E7"
+  };
